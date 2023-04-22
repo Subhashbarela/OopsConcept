76af8280613c7c7639a94a2f9e467a78bd9d7033
Subhash Barela <barelasubhash16@gmail.com>
2023-04-22 08:39:14 +0530

    push fist code

---
+++ ClassName.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopsConcepts
+{
+    public class ClassName
+    {
+        public void MethodName()
+        {
+            Console.WriteLine("Subhash barela");
+        }
+    }
+}
