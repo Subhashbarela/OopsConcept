7acee0a725bdaa9e666dc6807472d50d58f6a818
Subhash Barela <barelasubhash16@gmail.com>
2023-04-24 13:35:23 +0530

    Diplay the types of method

--- Program.cs
+++ Program.cs
@@ -10,28 +10,28 @@ namespace OopsConcepts
 
     public class Program
     {
-        public int id { get; set; }
-        public string name { get; set; }
-        public bool IsMarride { get; set; }
-        public double Heigth { get; set; }
-
-        public void Display()
+        public void NonParameterizeMethod()
         {
-            Console.WriteLine(" Student Id is: " + id);
-            Console.WriteLine(" StudentName is: " + name);
-            Console.WriteLine("Student Is Marride : " + IsMarride);
-            Console.WriteLine(" Student heigth is: " + Heigth);
-
+            Console.WriteLine("this is non parameteriase method");
         }
+        public void ParameterizeMethod(string name)
+        {
+            Console.WriteLine("this is  parameteriase method"+ name);
+        }
+        public static void StaticMethod()
+        {
+            Console.WriteLine("This is static method");
+        }
+
 
         static void Main(string[] args)
         { 
             Program Obj= new Program();
-            Obj.id = 102;
-            Obj.name = "Subhash";
-            Obj.IsMarride = false;
-            Obj.Heigth = 6.64;
-            Obj.Display();
+            Obj.NonParameterizeMethod();
+            Obj.ParameterizeMethod("subhash");
+            StaticMethod();
+          
+
             Console.Read();
         }
 
