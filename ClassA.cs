63b4f7d4169fa726607a8c6a0d24d373331a6b15
Subhash Barela <101850283+Subhashbarela@users.noreply.github.com>
2023-04-28 09:56:06 +0530

    Update ClassA.cs

--- ClassA.cs
+++ ClassA.cs
@@ -1,1 +1,17 @@
 
+ // Base Class 
+    public class ClassA
+    {
+        public void BaseClassMethod()
+        {
+            Console.WriteLine("This Is Base Class Method:..");
+        }
+    }
+    // Derive class 1
+    public class ClassB:ClassA
+    {
+        public void DeriveClassOneMethod()
+        {
+            Console.WriteLine("This Is derive Class Method:..");
+        }
+    }
