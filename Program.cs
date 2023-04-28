a4708dbe35734907ec3d5c51ab44c5ffb4abc07d
Subhash Barela <101850283+Subhashbarela@users.noreply.github.com>
2023-04-28 09:59:30 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,15 +6,6 @@ using System.Threading.Tasks;
 
 namespace OopsConcepts
 {
-    // Base Class
-    public class ClassA
-    {
-        public void BaseClassMethod()
-        {
-            Console.WriteLine("This Is Base Class Method:..");
-        }
-    }
-
     public class Program:ClassA
     {
         public void DeriveClassMethod()
