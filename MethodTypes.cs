b6506c9114ca4b1f824d59df4f2a7957c23375f6
Subhash Barela <101850283+Subhashbarela@users.noreply.github.com>
2023-04-28 10:37:04 +0530

    Create MethodTypes.cs

---
+++ MethodTypes.cs
@@ -0,0 +1,13 @@
+public void NonParameterizeMethod()
+        {
+            Console.WriteLine("this is non parameteriase method");
+        }
+        public void ParameterizeMethod(string name)
+        {
+            Console.WriteLine("this is  parameteriase method"+ name);
+        }
+        public static void StaticMethod()
+        {
+            Console.WriteLine("This is static method");
+        }
+
