dbc90c509790c4fc6921b76d6b31d99377d8991c
Subhash Barela <101850283+Subhashbarela@users.noreply.github.com>
2023-04-28 10:12:57 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -11,12 +11,18 @@ namespace OopsConcepts
 
             static void Main(string[] args)
             {
-                Shape s= new Shape();
-                s.draw();
-                s = new Rectangle();
-                s.draw();
-                s = new Circle();
-                s.draw();
+               //MethodOverLoading......
+            //MethodOverloading mol = new MethodOverloading();  
+            // mol.Add();
+            // mol.Add(10,20);
+            // mol.Add(5.4, 2.5);
+            // mol.Add("subhash", "barela");
+            
+            //MethodOverriding......
+            //MParentClass p=new MChildClassOne();  
+            //p.Print();
+            // MParentClass p = new MChildClassTwo();  //taking refrence of parent class create object of child class
+            // p.Print();
 
                 Console.Read();
             }
