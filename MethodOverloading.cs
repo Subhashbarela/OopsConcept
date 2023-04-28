72488a48ad64aa6635e97e66da5fd898610ee63a
Subhash Barela <101850283+Subhashbarela@users.noreply.github.com>
2023-04-28 10:11:34 +0530

    Update MethodOverloading.cs

--- MethodOverloading.cs
+++ MethodOverloading.cs
@@ -6,25 +6,29 @@ using System.Threading.Tasks;
 
 namespace OopsConcepts
 {
-    public class Shape
+    public class MethodOverloading
     {
-        public virtual void draw()
+        public void Add()
         {
-            Console.WriteLine("drawing...");
+            int a = 10;
+            int b = 20;
+            int c=a+b;
+            Console.WriteLine(c);
         }
-    }
-    public class Rectangle : Shape
-    {
-        public override void draw()
-        {
-            Console.WriteLine("drawing rectangle...");
+        public void Add(int a,int b)
+        {          
+            int c = a + b;
+            Console.WriteLine("Addition of two integer value: "+c);
         }
-    }
-    public class Circle : Shape
-    {
-        public override void draw()
+        public void Add(double a, double b)
         {
-            Console.WriteLine("drawing circle...");
+            double c = a + b;
+            Console.WriteLine("Addition of two float value: " + c);
+        }
+        public void Add(string a, string b)
+        {
+            string c = a +" "+ b;
+            Console.WriteLine("Concatinate of two string value: " + c);
         }
     }
     
