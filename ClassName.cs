5ea4b7b9f284e1eea0e6a6fd7806e76a71fce58d
Subhash Barela <101850283+Subhashbarela@users.noreply.github.com>
2023-04-28 10:27:05 +0530

    Create ClassName.cs

---
+++ ClassName.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopsConcept
+{
+    public class ClassName
+    {
+        //non parameterise method
+        public void MethodName()
+        {
+            Console.WriteLine("Subhash barela");
+        }
+        // paramiterize method
+        public void paramiterizeMethod(string name)
+        {
+            Console.WriteLine("Name: " + name);
+        }
+
+       
+    }
+}
