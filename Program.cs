dff6d9f7af646db2670b2387ca30e8142e3cb709
Subhash Barela <101850283+Subhashbarela@users.noreply.github.com>
2023-04-28 10:45:53 +0530

    Create class of ValueType and RefrenceType

--- Program.cs
+++ Program.cs
@@ -8,41 +8,23 @@ namespace OopsConcepts
 {
    public class Program
     {
-        public void ValueTypeMethod(int a) // this created seperate memory block for storing value
-        {
-            a += 10;
-            Console.WriteLine(" value of a is:"+a);
-        }
-        public void RefTypeMethod( ref int a) // this variable access address  for storing value
-        {
-            a += 10;
-            Console.WriteLine(" value of a is:" + a);
-        }
-        public void OutTypeMethod(out int a) // this variable access address  for storing value
-        {
-            a = 10;
-            Console.WriteLine(" value of a is:" + a);
-        }
-
-
-
-
-
+       
         static void Main(string[] args)
         { 
-            Program Obj= new Program();
+            ValueType Obj= new ValueType();
             int a = 5;
             Obj.ValueTypeMethod(a); //15
             Console.WriteLine("value of a in Main Method "+a); //5
            
             // Reference Type
+           RefrenceType Obj1 = new RefrenceType();
             int b = 5;  // send reference of this variable
-            Obj.RefTypeMethod(ref b); //15
+            Obj1.RefTypeMethod(ref b); //15
             Console.WriteLine("value of a in Main Method "+b); // 15
 
             //Out parameterise
             int c;  // send reference of this variable And no need to initialize value
-            Obj.OutTypeMethod(out c); //10
+            Obj1.OutTypeMethod(out c); //10
             Console.WriteLine("value of a in Main Method " + c); // 10
 
             Console.Read();
