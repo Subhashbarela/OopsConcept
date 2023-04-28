d64b4abf249d1179dcd16b86166146faefee686f
Subhash Barela <101850283+Subhashbarela@users.noreply.github.com>
2023-04-28 10:35:47 +0530

    Find Method type

--- Program.cs
+++ Program.cs
@@ -6,25 +6,9 @@ using System.Threading.Tasks;
 
 namespace OopsConcepts
 {
-   
-
-    public class Program
+   public class Program
     {
-        public void NonParameterizeMethod()
-        {
-            Console.WriteLine("this is non parameteriase method");
-        }
-        public void ParameterizeMethod(string name)
-        {
-            Console.WriteLine("this is  parameteriase method"+ name);
-        }
-        public static void StaticMethod()
-        {
-            Console.WriteLine("This is static method");
-        }
-
-
-        static void Main(string[] args)
+       static void Main(string[] args)
         { 
             Program Obj= new Program();
             Obj.NonParameterizeMethod();
