afce9765baf8d7de08e2777148a4bd4244a9f2e9
Subhash Barela <101850283+Subhashbarela@users.noreply.github.com>
2023-04-28 09:58:58 +0530

    Create ClassA.cs

---
+++ ClassA.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopsConcepts
+{
+    // Base Class
+    public class ClassA
+    {
+        public void BaseClassMethod()
+        {
+            Console.WriteLine("This Is Base Class Method:..");
+        }
+    }
+    
+ }
