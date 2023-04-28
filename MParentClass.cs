0408fa622c8b038888bb3b2c0c2787025bfcd548
Subhash Barela <101850283+Subhashbarela@users.noreply.github.com>
2023-04-28 10:08:34 +0530

    Add files via upload

---
+++ MParentClass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    public class MParentClass
+    {
+        //virtual keyword give authority to child class to modify method based on your need
+        public virtual void Print()
+        {
+            Console.WriteLine("This is PARENT class ");
+        }
+    }
+    public class MChildClassOne:MParentClass
+    {
+        public  override void Print()
+        {
+            Console.WriteLine("This is CHILD  ONE class ");
+        }
+    }
+    public class MChildClassTwo : MParentClass
+    {
+        public override void Print()
+        {
+            Console.WriteLine("This is CHILD  TWO class ");
+        }
+    }
+}
